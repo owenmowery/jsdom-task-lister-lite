e9db519638b18c16b82d13474be9519eda2764e8
Owen Mowery <owenmowery99@gmail.com>
2021-05-27 15:45:59 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let taskName = document.getElementById('new-task-description').value;
+    const ul = document.getElementById('tasks');
+    const li = document.createElement('li');
+    li.innerHTML = taskName;
+    ul.appendChild(li);
+    console.log(taskName);
+
+  })
+
 });
